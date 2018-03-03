4c24f0ed46d47f379ce1f53ac88fb332ba66087f
fpapouin <franck.papouin@gmail.com>
2018-03-03 15:00:07 -0800

    Added maggy

---
+++ maggy/maggyForm/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Karna.Magnification;
+using Gma.System.MouseKeyHook;
+using System.Diagnostics;
+
+namespace maggyForm
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        private KeyboardHook keyboardHook;
+        private int factorK = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.MouseDown += m_GlobalHook_MouseDown;
+            m_GlobalHook.MouseUp += m_GlobalHook_MouseUp;
+
+            keyboardHook = new KeyboardHook(true);
+            keyboardHook.KeyDown += Kh_KeyDown;
+
+            notifyIcon1.Icon = new Icon("appicon.ico");
+            if (!NativeMethods.MagInitialize())
+                throw new Exception();
+        }
+
+        private void Kh_KeyDown(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+            if (key == Keys.End) factorK = 1;
+            if (key == Keys.Next) factorK = 4;
+            if (key == Keys.PageUp) factorK = 8;
+            Debug.WriteLine("The Key: " + key);
+        }
+
+        private void m_GlobalHook_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right) zoom(factorK);
+        }
+
+        private void m_GlobalHook_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right) zoom(1);
+        }
+
+        private void zoom(int k)
+        {
+            float magnificationFactor = k;
+            int xDlg = (int)((float)NativeMethods.GetSystemMetrics(NativeMethods.SM_CXSCREEN) * (1.0 - (1.0 / magnificationFactor)) / 2.0);
+            int yDlg = (int)((float)NativeMethods.GetSystemMetrics(NativeMethods.SM_CYSCREEN) * (1.0 - (1.0 / magnificationFactor)) / 2.0);
+            NativeMethods.MagSetFullscreenTransform(magnificationFactor, xDlg, yDlg);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            NativeMethods.MagUninitialize();
+            m_GlobalHook.MouseDown -= m_GlobalHook_MouseDown;
+            m_GlobalHook.MouseUp -= m_GlobalHook_MouseUp;
+            keyboardHook.KeyDown -= Kh_KeyDown;
+            m_GlobalHook.Dispose();
+        }
+    }
+}
---
+++ maggy/maggyForm/NativeMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Karna.Magnification
+{
+    internal static class NativeMethods
+    {
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagInitialize();
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagUninitialize();
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagSetFullscreenTransform(float magLevel, int xOffset, int yOffset);
+    }
+}
