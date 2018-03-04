9d2e47cb228fad408b52e9a855ead6621de23220
fpapouin <franck.papouin@gmail.com>
2018-03-04 04:29:05 -0800

    Added vltream

---
+++ vltream/vltream/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace vltream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    throw new Exception(GetBadLenghtArgs());
+                }
+                else
+                {
+                    string filename = args[0];
+                    string width = "1280";
+                    string height = "720";
+                    string dst = "8081";
+                    string cmd = "\"" + @"C:\Program Files\VideoLAN\VLC\vlc.exe" + "\"";
+                    if (!File.Exists(cmd))
+                        cmd = "\"" + @"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" + "\"";
+
+                    if (Process.GetCurrentProcess().ProcessName.Contains("low"))
+                    {
+                        width = "640";
+                        height = "360";
+                    }
+                    if (Process.GetCurrentProcess().ProcessName.Contains("ulow"))
+                    {
+                        width = "320";
+                        height = "200";
+                    }
+
+                    if (args.Length > 1) width = args[1];
+                    if (args.Length > 2) height = args[2];
+                    if (args.Length > 3) dst = args[3];
+                    if (args.Length > 4) cmd = args[4];
+
+                    string cmdargs = "\"{0}\" ";
+                    cmdargs = cmdargs + "--sout=\"#transcode{vcodec=h264,vb=2000,venc=x264{profile=baseline},scale=Auto,";
+                    cmdargs = cmdargs + "width={1},height={2},acodec=mp3,ab=192,channels=2,samplerate=44100,scodec=dvbs,soverlay}";
+                    cmdargs = cmdargs + ":http{mux=ts,dst=:{3}/}\" --sout-keep";
+
+                    //cmdargs = String.Format(cmdargs, filename, width, height, dst);
+                    cmdargs = cmdargs.Replace("{0}", filename);
+                    cmdargs = cmdargs.Replace("{1}", width);
+                    cmdargs = cmdargs.Replace("{2}", height);
+                    cmdargs = cmdargs.Replace("{3}", dst);
+                    Console.WriteLine(cmd);
+                    Console.WriteLine(cmdargs);
+                    Process.Start(cmd, cmdargs);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey(true);
+            }
+        }
+
+        static string GetBadLenghtArgs()
+        {
+            string result = "Erreur dans les arguments";
+            result = result + "\n" + "args0=Filename.avi";
+            result = result + "\n" + "args1=1280 width (opt)";
+            result = result + "\n" + "args2=720 height (opt)";
+            result = result + "\n" + "args3=8081 dst (opt)";
+
+            return result;
+        }
+    }
+}
---
+++ vltream/vltream/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("vltream")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SAFRAN Sagem")]
+[assembly: AssemblyProduct("vltream")]
+[assembly: AssemblyCopyright("Copyright © SAFRAN Sagem 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("400be7d3-d7a5-4290-adef-ef6acc3719c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
