f66ed26e951b9efbbb0355e445c19030dfe619f2
fpapouin <fpapouin@siradel.com>
2019-07-31 10:37:40 +0200

    Backup iso and pause at end

--- PnGen/PnGen.py
+++ PnGen/PnGen.py
@@ -2,15 +2,20 @@ import sys
 import os
 import glob
 import subprocess
+import shutil
 
 def png_gen(txt_file, pallet_file):
     print('Processing {}'.format(txt_file))
     exe_path = os.path.join(r'\\siradel.local', 'Atlas', 'Radio', 'Resources', 'Softwares', 'Development', 'PnGen', 'PnGen.exe')
-    subprocess.run([exe_path, '-d', txt_file, '-p', pallet_file, '-qn', 'received_power', '-qu', 'db_mw', '-o', txt_file.replace('.txt', '.png')])
+    png_path = txt_file.replace('.txt', '.png')
+    if not os.path.exists(png_path.replace('.png', '.iso.png')):
+        shutil.copy(png_path, png_path.replace('.png', '.iso.png'))
+
+    subprocess.run([exe_path, '-d', txt_file, '-p', pallet_file, '-qn', 'received_power', '-qu', 'db_mw', '-o', png_path])
 
     results_dir = os.path.abspath(os.path.dirname(txt_file))
     references_dir = os.path.abspath(os.path.join(results_dir, '..', 'references'))
-    png_filename = os.path.basename(txt_file.replace('.txt', '.png'))
+    png_filename = os.path.basename(png_path)
     compare_png(references_dir, results_dir, png_filename)
 
 def compare_png(references_dir, results_dir, filename):
@@ -30,3 +35,4 @@ if __name__ == '__main__':
                 # get txt list from results folder
                 for f in glob.glob(os.path.join(root, dirname, '*.txt')):
                     png_gen(f, r'D:\svn\working directory\volcano_3_enterprise\Tests\resources\pallets\volcano_def.RES.vpf')
+    os.system('pause')
