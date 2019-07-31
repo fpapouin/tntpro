68896c1690a97d97830f0b7f42fa002a32bfee44
fpapouin <fpapouin@siradel.com>
2019-07-31 10:29:18 +0200

    Add PnGen.py

---
+++ PnGen/PnGen.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import glob
+import subprocess
+
+def png_gen(txt_file, pallet_file):
+    print('Processing {}'.format(txt_file))
+    exe_path = os.path.join(r'\\siradel.local', 'Atlas', 'Radio', 'Resources', 'Softwares', 'Development', 'PnGen', 'PnGen.exe')
+    subprocess.run([exe_path, '-d', txt_file, '-p', pallet_file, '-qn', 'received_power', '-qu', 'db_mw', '-o', txt_file.replace('.txt', '.png')])
+
+    results_dir = os.path.abspath(os.path.dirname(txt_file))
+    references_dir = os.path.abspath(os.path.join(results_dir, '..', 'references'))
+    png_filename = os.path.basename(txt_file.replace('.txt', '.png'))
+    compare_png(references_dir, results_dir, png_filename)
+
+def compare_png(references_dir, results_dir, filename):
+    exe_path = os.path.join(r'\\siradel.local', 'Atlas', 'PROJECTS', '201206 InteractiveViewer3D', 'Product', 'Libraries', 'ImageComparator', 'master', '9-4265e76e2e', 'Release', 'CompareImageFiles.exe')
+    print(filename)
+    # compare with png from res folder
+    # output norm diff in res folder
+    subprocess.run([exe_path, os.path.join(references_dir, filename), os.path.join(results_dir, filename), os.path.join(results_dir, filename + '_diff.png'), '100', '0'])
+
+if __name__ == '__main__':
+    scenarii_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Iterate scenarii results
+    for root, dirs, files in os.walk(scenarii_dir):
+        for dirname in dirs:
+            if dirname == 'Results' or dirname == 'results':
+                # get txt list from results folder
+                for f in glob.glob(os.path.join(root, dirname, '*.txt')):
+                    png_gen(f, r'D:\svn\working directory\volcano_3_enterprise\Tests\resources\pallets\volcano_def.RES.vpf')
