26fa29391fb44c4190104e79bf60c1a8cd06c1c8
fpapouin <franck.papouin@gmail.com>
2019-11-24 01:07:15 +0100

    Good ranks

--- ranks/Form1.Designer.cs
+++ ranks/Form1.Designer.cs
@@ -28,23 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
-            // flowLayoutPanel1
+            // tableLayoutPanel1
             // 
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1187, 364);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(625, 292);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1187, 364);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(625, 292);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Ranks";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -53,8 +58,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- ranks/Form1.cs
+++ ranks/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,39 +12,229 @@ using System.Windows.Forms;
 
 namespace ranks
 {
+    struct Profile
+    {
+        public int wingman;
+        public int compet;
+        public string url;
+    }
+
     public partial class Form1 : Form
     {
+        readonly Dictionary<string, Profile> Accounts = new Dictionary<string, Profile>();
+        readonly Dictionary<int, string> WingmanRanks = new Dictionary<int, string>();
+        readonly Dictionary<int, string> Ranks = new Dictionary<int, string>();
+
         public Form1()
         {
             InitializeComponent();
+            BackColor = Color.Black;
+            WingmanRanks.Add(0, "unrank-wingman.png");
+            WingmanRanks.Add(1, "silver-1-wingman.png");
+            WingmanRanks.Add(2, "silver-2-wingman.png");
+            WingmanRanks.Add(3, "silver-3-wingman.png");
+            WingmanRanks.Add(4, "silver-4-wingman.png");
+            WingmanRanks.Add(5, "silver-5-wingman.png");
+            WingmanRanks.Add(6, "silver-elite-master-wingman.png");
+            WingmanRanks.Add(7, "gold-nova-wingman.png");
+            WingmanRanks.Add(8, "gold-nova-2-wingman.png");
+            WingmanRanks.Add(9, "gold-nova-3-wingman.png");
+            WingmanRanks.Add(10, "Gold-nova-master-wingman.png");
+            WingmanRanks.Add(11, "master-guardian-1-wingman.png");
+            WingmanRanks.Add(12, "master-guardian-2-wingman.png");
+            WingmanRanks.Add(13, "master-guardian-elite-wingman.png");
+            WingmanRanks.Add(14, "dinstinguished-master-guardian-wingman.png");
+            WingmanRanks.Add(15, "legendary-eagle-wingman.png");
+            WingmanRanks.Add(16, "legendary-eagle-master-wingman.png");
+            WingmanRanks.Add(17, "supreme-elite-master-class-wingman.png");
+            WingmanRanks.Add(18, "Global-elite-wingman.png");
+            Ranks.Add(0, "unranked.png");
+            Ranks.Add(1, "silver-1.png");
+            Ranks.Add(2, "silver2.png");
+            Ranks.Add(3, "silver-3.png");
+            Ranks.Add(4, "silver4-csgo.png");
+            Ranks.Add(5, "silver-5.png");
+            Ranks.Add(6, "silver-6.png");
+            Ranks.Add(7, "nova-1.png");
+            Ranks.Add(8, "nova-2.png");
+            Ranks.Add(9, "nova-3.png");
+            Ranks.Add(10, "nova4.png");
+            Ranks.Add(11, "master-gardian.png");
+            Ranks.Add(12, "master-gardian-2.png");
+            Ranks.Add(13, "master-gardian-elite.png");
+            Ranks.Add(14, "sherif.png");
+            Ranks.Add(15, "legendary-eagle.png");
+            Ranks.Add(16, "legendary-eagle-master.png");
+            Ranks.Add(17, "supreme-elite-master-class.png");
+            Ranks.Add(18, "global-elite.png");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            BackColor = Color.Black;
+            tableLayoutPanel1.ColumnCount = 8;
+            //tableLayoutPanel1.RowCount = 0;
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            LoadAccounts();
+
             if (Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "png\\")))
-                AddButtons();
+            {
+                FindAvatar();
+                AddAvatarButtons();
+            }
+            WindowState = FormWindowState.Maximized;
+            //SaveAccounts();
+            SaveRanks();
+            Application.Exit();
         }
 
-        private void AddButtons()
+        private void SaveRanks()
         {
-            foreach (string fileName in Directory.GetFiles("png", "*png"))
+            int minux = 200;
+            using (Bitmap bmp = new Bitmap(tableLayoutPanel1.Width - minux, tableLayoutPanel1.Height - 70))
             {
+                tableLayoutPanel1.DrawToBitmap(bmp, new Rectangle(Point.Empty, bmp.Size));
+                bmp.Save(@"ranks1.png", ImageFormat.Png);
+            }
+            for (int i = 0; i < 8 * 9; i++)
+            {
+                tableLayoutPanel1.Controls.RemoveAt(0);
+            }
+
+            using (Bitmap bmp = new Bitmap(tableLayoutPanel1.Width - minux, tableLayoutPanel1.Height - 70))
+            {
+                tableLayoutPanel1.DrawToBitmap(bmp, new Rectangle(Point.Empty, bmp.Size));
+                bmp.Save(@"ranks2.png", ImageFormat.Png);
+            }
+            for (int i = 0; i < 8 * 9; i++)
+            {
+                tableLayoutPanel1.Controls.RemoveAt(0);
+            }
+            using (Bitmap bmp = new Bitmap(tableLayoutPanel1.Width - minux, tableLayoutPanel1.Height - 70 * 5))
+            {
+                tableLayoutPanel1.DrawToBitmap(bmp, new Rectangle(Point.Empty, bmp.Size));
+                bmp.Save(@"ranks3.png", ImageFormat.Png);
+            }
+            Image img1 = Image.FromFile("ranks1.png");
+            Image img2 = Image.FromFile("ranks2.png");
+            Image img3 = Image.FromFile("ranks3.png");
+            using (Bitmap bmp = new Bitmap(img1.Width, img1.Height + img2.Height + img3.Height))
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawImage(img1, 0, 0);
+                g.DrawImage(img2, 0, img1.Height);
+                g.DrawImage(img3, 0, img1.Height + img2.Height);
+                bmp.Save(@"ranks.png", ImageFormat.Png);
+            }
+        }
+
+        private void LoadAccounts()
+        {
+            foreach (string data in File.ReadAllText("ranks.txt").Replace("\r", "").Split("`".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (data.Replace("\n", "") != string.Empty)
+                {
+                    string[] s = data.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    if (Accounts.ContainsKey(s[0]))
+                    {
+                        Accounts[s[0]] = new Profile() { wingman = int.Parse(s[1]), compet = int.Parse(s[2]), url = s[3] };
+                    }
+                    else
+                    {
+                        Accounts.Add(s[0], new Profile() { wingman = int.Parse(s[1]), compet = int.Parse(s[2]), url = s[3] });
+                    }
+                }
+            }
+        }
+
+        private void SaveAccounts()
+        {
+            string s = string.Empty;
+            foreach (var account in Accounts)
+            {
+                s += account.Key + Environment.NewLine + account.Value.wingman + Environment.NewLine + account.Value.compet + Environment.NewLine + account.Value.url + Environment.NewLine + "`" + Environment.NewLine;
+            }
+            File.WriteAllText("ranks.txt", s);
+        }
+
+        private void AddAvatarButtons()
+        {
+            int i = 0;
+            foreach (var account in Accounts.OrderByDescending(a => a.Value.compet).ThenByDescending(a => a.Value.wingman))
+            {
+                i++;
+
+                Button r = new Button();
+                tableLayoutPanel1.Controls.Add(r);
+                r.Height = 100;
+                r.Width = 200;
+                r.TabStop = false;
+                r.FlatStyle = FlatStyle.Flat;
+                r.FlatAppearance.BorderSize = 0;
+                r.Name = account.Key;
+                r.Text = "" + i;
+                r.TextAlign = ContentAlignment.MiddleRight;
+                r.ForeColor = Color.White;
+                r.Font = new Font(r.Font.FontFamily, 70);
+
                 Button b = new Button();
-                flowLayoutPanel1.Controls.Add(b);
+                tableLayoutPanel1.Controls.Add(b);
                 b.Height = 100;
                 b.Width = 200;
-                if (Screen.PrimaryScreen.Bounds.Width < 1900)
-                {
-                    b.Height = 150;
-                    b.Width = 150;
-                }
-                b.BackgroundImage = Image.FromFile(fileName);
+                b.BackgroundImage = Image.FromFile("png/" + account.Key + ".png");
                 b.BackgroundImageLayout = ImageLayout.Zoom;
                 b.TabStop = false;
                 b.FlatStyle = FlatStyle.Flat;
                 b.FlatAppearance.BorderSize = 0;
-                b.Name = fileName.Replace("\\", "").Replace("png", "").Replace(".", "");
+                b.Name = account.Key;
+
+                //Add compet
+                Button c = new Button();
+                tableLayoutPanel1.Controls.Add(c);
+                c.Height = 100;
+                c.Width = 200;
+                c.BackgroundImage = Image.FromFile("png/" + Ranks[account.Value.compet]);
+                c.BackgroundImageLayout = ImageLayout.Zoom;
+                c.TabStop = false;
+                c.FlatStyle = FlatStyle.Flat;
+                c.FlatAppearance.BorderSize = 0;
+                c.Name = account.Key;
+
+                //Add wingman
+                Button w = new Button();
+                tableLayoutPanel1.Controls.Add(w);
+                w.Height = 100;
+                w.Width = 200;
+                w.BackgroundImage = Image.FromFile("png/" + WingmanRanks[account.Value.wingman]);
+                w.BackgroundImageLayout = ImageLayout.Zoom;
+                w.TabStop = false;
+                w.FlatStyle = FlatStyle.Flat;
+                w.FlatAppearance.BorderSize = 0;
+                w.Name = account.Key;
+            }
+        }
+
+        private void FindAvatar()
+        {
+            foreach (var account in Accounts)
+            {
+                string avatarFileName = "png/" + account.Key + ".png";
+                if (!File.Exists(avatarFileName))
+                {
+                    using (System.Net.WebClient wc = new System.Net.WebClient())
+                    {
+                        string webData = wc.DownloadString(account.Value.url);
+                        foreach (string line in webData.Split('\n'))
+                        {
+                            if (line.Contains("playerAvatarAutoSizeInner"))
+                            {
+                                int ss = line.IndexOf("https");
+                                int se = line.IndexOf(".jpg");
+                                wc.DownloadFile(line.Substring(ss, se - ss + 4), avatarFileName);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
