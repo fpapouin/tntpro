69bfa936b297e5980b5cbae957670b8d82d6ed17
fpapouin <franck.papouin@gmail.com>
2020-03-31 19:52:52 +0200

    test alive

---
+++ alive/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace alive
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            this.notifyIcon1.Icon = this.Icon;
+            this.notifyIcon1.Text = "alive";
+            System.Timers.Timer timer = new System.Timers.Timer(1000*60);
+            timer.Elapsed += OnTimedEvent;
+            timer.Start();
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            (source as System.Timers.Timer).Stop();
+            keybd_event((byte)Keys.PrintScreen, 0, 0, 0);
+            keybd_event((byte)Keys.PrintScreen, 0, 2, 0);
+            (source as System.Timers.Timer).Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- oneTap3/alive.py
+++ oneTap3/alive.py
@@ -1,6 +1,8 @@
 # pip install pyautogui
-import pyautogui, pytweening
-import sys, random
+import pyautogui
+import pytweening
+import sys
+import random
 import time
 
 
@@ -10,17 +12,27 @@ def fake_move():
         rpoints.append([random.randint(-10, 10), random.randint(-5, 5)])
 
     for p in rpoints:
-        pyautogui.moveRel(+p[0], +p[1], tween=pytweening.easeInOutBack, duration=0.0)
+        pyautogui.moveRel(+p[0], +p[1],
+                          tween=pytweening.easeInOutBack, duration=0.0)
 
     for p in rpoints:
-        pyautogui.moveRel(-p[0], -p[1], tween=pytweening.easeInOutBack, duration=0.0)
+        pyautogui.moveRel(-p[0], -p[1],
+                          tween=pytweening.easeInOutBack, duration=0.0)
 
 
 def do_loop():
-    time.sleep(300)
-    pyautogui.mouseDown()
-    #fake_move()
-    pyautogui.mouseUp()
+    # time.sleep(60*20)
+    # pyautogui.mouseDown()
+    # fake_move()
+    # pyautogui.mouseUp()
+    # pyautogui.keyDown('prtscr')
+    # time.sleep(0.5)
+    # pyautogui.keyUp('prtscr')
+    #time.sleep(3)
+    pyautogui.moveTo(1920/2, 1080/2-100, 1, pytweening.easeInOutQuint, 1)
+    #time.sleep(3)
+    pyautogui.moveTo(1920/2-100, 1080/2, 1, pytweening.easeInOutQuint, 1)
+
 
 while True:
     do_loop()
